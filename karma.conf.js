6b10aa6845880de186cf7dd7d201ca9defa5fd28
Robert Velickovski <robert.velickovski@q.agency>
2025-06-19 17:58:08 +0200

    v2.0.1
    - test update

--- karma.conf.js
+++ karma.conf.js
@@ -1,45 +1,39 @@
-process.env.CHROME_BIN = require('puppeteer').executablePath();
+process.env.CHROME_BIN = require("puppeteer").executablePath();
 
 module.exports = function (config) {
+	"use strict";
 
-    'use strict';
-
-    config.set({
-        port: 9876,
-        autoWatch: false,
-        singleRun: true,
-        browsers: ['ChromeHeadless'],
-		frameworks: ['mocha', 'chai', 'sinon'],
-        reporters: ['mocha', 'coverage'],
-        files: [
-            'https://code.jquery.com/jquery-2.2.4.min.js',
-        ],
-        preprocessors: {
-			'**/src/js/*.js': 'coverage',
-			'src/js/*.js': ['babel'],
-			'test/*.js': ['babel']
-        },
-        babelPreprocessor: {
-			options: {
-			  	presets: ['@babel/preset-env'],
-			  	sourceMap: 'inline'
-			}
+	config.set({
+		port: 9876,
+		autoWatch: false,
+		singleRun: true,
+		browsers: ["ChromeHeadless"],
+		frameworks: ["mocha", "chai", "sinon"],
+		reporters: ["mocha", "coverage"],
+		files: ["https://code.jquery.com/jquery-2.2.4.min.js"],
+		preprocessors: {
+			"**/src/js/*.js": "coverage",
+			"src/js/*.js": ["babel"],
+			"test/*.js": ["babel"],
 		},
-        coverageReporter: {
-            type : 'html',
-            dir : 'coverage/',
-            subdir: 'report',
-            reporters: [
-                { type: 'html' },
-                { type: 'text-summary' }
-            ],
-            includeAllSources: true,
-            watermarks: {
-                statements: [50, 90],
-                functions: [50, 90],
-                branches: [50, 90],
-                lines: [50, 90]
-            }
-		}
-    });
+		babelPreprocessor: {
+			options: {
+				presets: ["@babel/preset-env"],
+				sourceMap: "inline",
+			},
+		},
+		coverageReporter: {
+			type: "html",
+			dir: "coverage/",
+			subdir: "report",
+			reporters: [{ type: "html" }, { type: "text-summary" }],
+			includeAllSources: true,
+			watermarks: {
+				statements: [50, 90],
+				functions: [50, 90],
+				branches: [50, 90],
+				lines: [50, 90],
+			},
+		},
+	});
 };
--- test/index.js
+++ test/index.js
@@ -1,35 +1,36 @@
-describe('jPopup', () => {
+describe("jPopup", () => {
+	const openClass = "jPopup--isOpen";
 
-    const openClass = 'jPopup--isOpen';
+	var dummyPopup, dummyContent;
 
-    var dummyPopup,
-        dummyContent;
+	before(() => {
+		dummyContent = "<h1>Title</h1>";
+		dummyPopup = new jPopup({
+			content: dummyContent,
+		});
+	});
 
-    before(() => {
+	describe("open()", () => {
+		it("should open new popup with passed content", () => {
+			dummyPopup.open();
 
-        dummyContent = '<h1>Title</h1>';
-        dummyPopup = new jPopup({
-            content: dummyContent
-        });
-    });
+			expect(
+				document.querySelector("html").classList.contains(openClass)
+			).equal(true);
+			expect(document.querySelectorAll(".jPopup").length).equal(1);
+			expect(document.querySelector(".jPopup-content").innerHTML).equal(
+				dummyContent
+			);
+		});
+	});
 
-    describe('open()', () => {
+	describe("close()", () => {
+		it("should remove popup element from DOM", () => {
+			dummyPopup.close();
 
-        it('should open new popup with passed content', () => {
-            dummyPopup.open();
-
-            expect(document.querySelector('html').classList.contains(openClass)).equal(true);
-            expect(document.querySelectorAll('.jPopup').length).equal(1);
-            expect(document.querySelector('.jPopup-content').innerHTML).equal(dummyContent);
-        });
-    });
-
-    describe('close()', () => {
-
-        it('should remove popup element from DOM', () => {
-            dummyPopup.close();
-
-            expect(document.querySelector('html').classList.contains(openClass)).equal(false);
-        });
-    });
+			expect(
+				document.querySelector("html").classList.contains(openClass)
+			).equal(false);
+		});
+	});
 });
